19be3bc8d0d96825f02de149831c0f960ae5b1d0
arolleaguekeng <arolleaguekeng@gmail.com>
2024-07-25 01:21:53 +0100

    update readme and models list

--- libs/ng-flexbot/src/lib/ng-flexbot/models/flexbot-current-llm.enum.ts
+++ libs/ng-flexbot/src/lib/ng-flexbot/models/flexbot-current-llm.enum.ts
@@ -1,15 +1,24 @@
 export enum FlexbotCurrentTextModel {
+  // Google Models
   GOOGLE_GEMINI_PRO = 'google/gemini-pro',
-  OPEN_AI_GPT3 = 'openai/gpt-3',
-  OPEN_AI_GPT3_CHAT = 'openai/gpt-3-chat',
-  OPEN_AI_GPT_4o_MINI = 'openai/gpt-4o-mini',
-  OPEN_AI_GPT3_5_TURBO = 'openai/gpt-3.5-turbo',
+  GOOGLE_MEDULLA_2 = 'google/medulla-2', 
+
+
+  // OpenAI Models
+  OPEN_AI_GPT_3 = 'openai/gpt-3',
+  OPEN_AI_GPT_3_CHAT = 'openai/gpt-3-chat',
+  OPEN_AI_GPT_4 = 'openai/gpt-4',
+  OPEN_AI_GPT_4_TURBO = 'openai/gpt-4-turbo',
+  OPEN_AI_GPT_3_5_TURBO = 'openai/gpt-3.5-turbo',
+  OPEN_AI_TEXT_DAVINCI_003 = 'openai/text-davinci-003', 
 }
 
+export enum FlexbotCurrentImageModel {
+  // Google Models
+  GOOGLE_GEMINI_PRO_VISION = 'google/gemini-pro-vision',
 
-
-export enum FlexbotCurrentImageModel{
-    GOOGLE_GEMINI_PRO_VISION = 'google/gemini-pro-vision',
-    OPEN_AI_DALL_E_3 = 'openai/dall-e-3',
-    OPEN_AI_GPT_4o_MINI = 'openai/gpt-4o-mini',
+  // OpenAI Models
+  OPEN_AI_DALL_E_3 = 'openai/dall-e-3',
+  OPEN_AI_DALL_E_2 = 'openai/dall-e-2', // Example of another potential OpenAI image model
+  // Add more OpenAI image models as they become available
 }
