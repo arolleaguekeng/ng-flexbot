174bbcc029115564bd4e8ec060f3f4732701c83f
arolleaguekeng <arolleaguekeng@gmail.com>
2024-07-26 02:13:29 +0100

    update chat design

--- libs/ng-flexbot/src/lib/ng-flexbot/ng-flexbot.component.ts
+++ libs/ng-flexbot/src/lib/ng-flexbot/ng-flexbot.component.ts
@@ -38,7 +38,65 @@ export class NgFlexbotComponent implements OnInit {
   }
 
   isLoading = false;
-  chatMessages: ChatModel[] = [];
+  chatMessages: ChatModel[] = [
+    new ChatModel(
+      this.welcomeMessage,
+      new Date(),
+      '',
+      MessageType.BOT_MESSAGE
+    ),
+
+    new ChatModel(
+      'I am Flexbot, a language model that can generate text based on the input you provide. You can ask me questions, tell me stories, or even ask me to generate text based on an image. How can I help you today?',
+      new Date(),
+      '',
+      MessageType.USER_MESSAGE
+    ),
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.BOT_MESSAGE
+    ),
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.USER_MESSAGE
+    ),
+
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.BOT_MESSAGE
+    ),
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.USER_MESSAGE
+    ),
+
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.BOT_MESSAGE
+    ),
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.USER_MESSAGE
+    ),
+    new ChatModel(
+      'You can also ask me to generate text based on a video. Just provide me with a YouTube link and ask me a question or tell me what you want me to generate text about.',
+      new Date(),
+      '',
+      MessageType.BOT_MESSAGE
+    ),
+  ];
   currentChatItem: ChatModel = new ChatModel(
     '',
     new Date(),
