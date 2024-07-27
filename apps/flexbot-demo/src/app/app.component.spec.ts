145d2722d69959c03a43ff71ae5818f49c184ad8
arolleaguekeng <arolleaguekeng@gmail.com>
2024-07-27 16:26:42 +0100

    flexbot demo unit tests

--- apps/flexbot-demo/src/app/app.component.spec.ts
+++ apps/flexbot-demo/src/app/app.component.spec.ts
@@ -1,27 +1,49 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { FlexbotCurrentTextModel, FlexbotCurrentImageModel } from 'libs/ng-flexbot/src/lib/ng-flexbot/models/flexbot-current-llm.enum';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, HttpClientTestingModule], 
+      providers: [
+        { provide: environment, useValue: environment },
+      ],
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain(
-  //     'Welcome flexbot-demo'
-  //   );
-  // });
+  it('should create the app component', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it(`should have as title 'flexbot-demo'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('flexbot-demo');
-  // });
+  it(`should have as title 'flexbot-demo'`, () => {
+    expect(component.title).toEqual('flexbot-demo');
+  });
+
+  it('should have googleApikey from environment', () => {
+    expect(component.googleApikey).toEqual(environment.apikey);
+  });
+
+  it('should have openaiApikey from environment', () => {
+    expect(component.openaiApikey).toEqual(environment.openaiApikey);
+  });
+
+  it('should have a default promptContext', () => {
+    expect(component.promptContext).toEqual('');
+  });
+
+  it('should have the correct default text model', () => {
+    expect(component.textModel).toEqual(FlexbotCurrentTextModel.GOOGLE_GEMINI_PRO);
+  });
+
+  it('should have the correct default image model', () => {
+    expect(component.imageModel).toEqual(FlexbotCurrentImageModel.GOOGLE_GEMINI_1_5_FLASH);
+  });
 });
--- apps/server.js
+++ apps/server.js
@@ -6,22 +6,9 @@ const dir = "environments";
 const file = "environment.ts";
 const prodFile = "environment.prod.ts"; // For production deployment
 
-const content = `${process.env.ANGULAR_ENV}`;
+const content = `${process.env.ANGULAR_ENV || ''}`;
 
 fs.access(dir, fs.constants.F_OK, (err) => {
-    if (err) {
-        // Directory doesn't exist
-        console.log("src doesn't exist, creating now", process.cwd());
-        // Create /src
-        try {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        catch (error) {
-            console.log(`Error while creating ${dir}. Error is ${error}`);
-            process.exit(1);
-        }
-    }
-    // Now write to file
     try {
         fs.writeFileSync(dir + "/" + file, content);
         fs.writeFileSync(dir + "/" + prodFile, content);
