b2e38b37a36bd5567c5bcc1bd38c603c08895211
arolleaguekeng <arolleaguekeng@gmail.com>
2024-07-27 16:35:22 +0100

    chore: Refactor server.js to use fs.promises for asynchronous file operations

--- apps/server.js
+++ apps/server.js
@@ -1,5 +1,4 @@
-/* server.js in root directory */
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const dir = "environments";
@@ -8,18 +7,37 @@ const prodFile = "environment.prod.ts"; // For production deployment
 
 const content = `${process.env.ANGULAR_ENV || ''}`;
 
-fs.access(dir, fs.constants.F_OK, (err) => {
+async function createEnvironmentFiles() {
     try {
-        fs.writeFileSync(dir + "/" + file, content);
-        fs.writeFileSync(dir + "/" + prodFile, content);
+        await fs.access(dir);
+    } catch (err) {
+        console.log(`${dir} doesn't exist, creating now`, process.cwd());
+        try {
+            await fs.mkdir(dir, { recursive: true });
+        } catch (error) {
+            console.error(`Error while creating ${dir}. Error: ${error.message}`);
+            process.exit(1);
+        }
+    }
+
+    try {
+        const filePath = path.join(dir, file);
+        const prodFilePath = path.join(dir, prodFile);
+
+        await fs.writeFile(filePath, content);
+        await fs.writeFile(prodFilePath, content);
+
         console.log("Created successfully in", process.cwd());
-        if (fs.existsSync(dir + "/" + file)) {
-            console.log("File is created", path.resolve(dir + "/" + file));
-            const str = fs.readFileSync(dir + "/" + file).toString();
+
+        if (await fs.access(filePath)) {
+            console.log("File is created", path.resolve(filePath));
+            const str = await fs.readFile(filePath, 'utf8');
             console.log(str);
         }
     } catch (error) {
-        console.error(error);
+        console.error(`Error while writing files. Error: ${error.message}`);
         process.exit(1);
     }
-});
+}
+
+createEnvironmentFiles();
